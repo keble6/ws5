f98da15c4c5fc543c6a2dabca092fd8caf1a57c5
Rob Wilson <rob@rwilson.org.uk>
2021-01-15 09:51:15 +0000

    Working with basic forever readings

--- main.ts
+++ main.ts
@@ -20,24 +20,12 @@ input.onButtonPressed(Button.A, function () {
         . . # . .
         . . # . .
         `)
-    if (readingsLength != 0) {
-        for (let index = 0; index <= readingsLength - 1; index++) {
-            bluetooth.uartWriteString(dateTimeReadings[index])
-            basic.pause(20)
-            bluetooth.uartWriteString(airPressureReadings[index])
-            basic.pause(20)
-            bluetooth.uartWriteLine(temperatureReadings[index])
-            basic.pause(20)
-        }
-    }
-    basic.showIcon(IconNames.Yes)
-    readingsLength = dateTimeReadings.length
 })
-function airPressure () {
-    BMP280.PowerOn()
-    pressure = BMP280.pressure()
-    BMP280.PowerOff()
-    return "" + pressure / 100 + ","
+function getReadings () {
+    BME280.PowerOn()
+    readings = "" + BME280.pressure(BME280_P.Pa) + "," + BME280.temperature(BME280_T.T_C) + "," + BME280.humidity()
+    BME280.PowerOff()
+    return readings
 }
 function dateTimeString () {
     return "" + leadingZero(DS3231.date()) + "/" + leadingZero(DS3231.month()) + "/" + DS3231.year() + " " + leadingZero(DS3231.hour()) + ":" + leadingZero(DS3231.minute()) + ","
@@ -45,51 +33,26 @@ function dateTimeString () {
 // Press both A & B to set clock
 input.onButtonPressed(Button.AB, function () {
     DS3231.dateTime(
-    2020,
-    9,
-    14,
+    2021,
     1,
     15,
-    0,
+    1,
+    9,
+    7,
     0
     )
     basic.showIcon(IconNames.Yes)
 })
 // Delete readings array
 input.onButtonPressed(Button.B, function () {
-    readingsLength = dateTimeReadings.length
-    for (let index = 0; index < readingsLength; index++) {
-        dateTimeReadings.removeAt(0)
-        airPressureReadings.removeAt(0)
-        temperatureReadings.removeAt(0)
-    }
+	
 })
-function temperature () {
-    dht11_dht22.queryData(
-    DHTtype.DHT11,
-    DigitalPin.P1,
-    true,
-    false,
-    true
-    )
-    if (dht11_dht22.sensorrResponding()) {
-        return "" + dht11_dht22.readData(dataType.temperature) + "," + dht11_dht22.readData(dataType.humidity)
-    } else {
-        // Warn if DHT11 error
-        return "No DHT11 response!"
-    }
-}
-let pressure = 0
-let readingsLength = 0
-let temperatureReadings: string[] = []
-let airPressureReadings: string[] = []
-let dateTimeReadings: string[] = []
+let readings = ""
 bluetooth.startUartService()
 // This is the maximum number of records in the readings array
 let readingsMax = 200
-dateTimeReadings = []
-airPressureReadings = []
-temperatureReadings = []
+let dateTimeReadings: number[] = []
+let weatherReadings: number[] = []
 DS3231.configureINTCN(interruptEnable.Enable)
 DS3231.clearAlarmFlag(alarmNum.A1)
 DS3231.clearAlarmFlag(alarmNum.A2)
@@ -103,25 +66,13 @@ mode.Minute,
 )
 DS3231.disableAlarm(alarmNum.A1, interruptEnable.Enable)
 DS3231.disableAlarm(alarmNum.A2, interruptEnable.Disable)
-// Send initial readings to USB as a test
-serial.writeLine("" + (dateTimeString()))
-serial.writeLine("" + (airPressure()))
-serial.writeLine("" + (temperature()))
 // Poll pin P0 to see if alarm is set
 basic.forever(function () {
-    // Check if the alarm has triggered
-    if (pins.digitalReadPin(DigitalPin.P0) == 0 && DS3231.status() == 9) {
-        // Limit the number of stored readings
-        if ([0].length < readingsMax) {
-            dateTimeReadings.push(dateTimeString())
-            airPressureReadings.push(airPressure())
-            temperatureReadings.push(temperature())
-            DS3231.clearAlarmFlag(alarmNum.A1)
-        }
-    }
-    // Display the number of stored readings
-    basic.showNumber(dateTimeReadings.length)
+    basic.showIcon(IconNames.Heart)
     basic.pause(100)
     basic.clearScreen()
-    basic.pause(19900)
+    basic.pause(9900)
+    serial.writeString("" + (dateTimeString()))
+    serial.writeLine("" + (getReadings()))
+    BME280.PowerOff()
 })
