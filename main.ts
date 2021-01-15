17e7f8bbb39e62aef3176b3e4dac43e4fe430ed0
Rob Wilson <rob@rwilson.org.uk>
2021-01-15 11:20:53 +0000

    Added button A full code

--- main.ts
+++ main.ts
@@ -23,15 +23,17 @@ input.onButtonPressed(Button.A, function () {
     readingsLength = dateTimeReadings.length
     if (readingsLength != 0) {
         for (let index = 0; index <= readingsLength - 1; index++) {
-            serial.writeString("" + (dateTimeString()))
-            serial.writeLine("" + (getReadings()))
+            bluetooth.uartWriteString(dateTimeString())
+            basic.pause(10)
+            bluetooth.uartWriteLine(getReadings())
+            basic.pause(10)
         }
     }
     basic.showIcon(IconNames.Yes)
 })
 function getReadings () {
     BME280.PowerOn()
-    readings = "" + BME280.pressure(BME280_P.Pa) + "," + BME280.temperature(BME280_T.T_C) + "," + BME280.humidity()
+    readings = "" + BME280.pressure(BME280_P.hPa) + "," + BME280.temperature(BME280_T.T_C) + "," + BME280.humidity()
     BME280.PowerOff()
     return readings
 }
@@ -56,7 +58,7 @@ input.onButtonPressed(Button.B, function () {
 	
 })
 let readings = ""
-let readingsLength: number = []
+let readingsLength = 0
 let dateTimeReadings: string[] = []
 bluetooth.startUartService()
 // This is the maximum number of records in the readings array
@@ -78,13 +80,17 @@ DS3231.disableAlarm(alarmNum.A1, interruptEnable.Enable)
 DS3231.disableAlarm(alarmNum.A2, interruptEnable.Disable)
 // Poll pin P0 to see if alarm is set
 basic.forever(function () {
-    basic.showIcon(IconNames.Heart)
+    if (pins.digitalReadPin(DigitalPin.P0) == 0 && DS3231.status() == 9) {
+        if (dateTimeReadings.length <= readingsMax) {
+            dateTimeReadings.push(dateTimeString())
+            weatherReadings.push(getReadings())
+            DS3231.clearAlarmFlag(alarmNum.A1)
+        }
+    }
     basic.showNumber(dateTimeReadings.length)
     basic.pause(100)
     basic.clearScreen()
     basic.pause(9900)
-    dateTimeReadings.push(dateTimeString())
-    weatherReadings.push(getReadings())
     serial.writeString("" + (dateTimeString()))
     serial.writeLine("" + (getReadings()))
     BME280.PowerOff()
